27e0a0f891465828bff90292282a01420707b68a
Scott <ser.arroz@gmail.com>
2025-01-10 03:15:13 -0700

    Update voidriver.sh

--- voidriver.sh
+++ voidriver.sh
@@ -27,13 +27,13 @@ sudo xbps-install -Sy git wget base-devel xtools mesa-dri stow nerd-fonts bash-c
 # Wayland Specific
 sudo xbps-install -Sy linux-firmware-intel mesa-dri wl-clipboard grim slurp imv yazi mpv bat|| { echo "Package installation failed."; exit 1; }
 # River / DE
-sudo xbps-install -Sy river gdm fuzzel yambar wlr-randr kanshi swaylock swayidle qutebrowser || { echo "Package installation failed."; exit 1; }
+sudo xbps-install -Sy river gdm fuzzel yambar wlr-randr kanshi swaylock swayidle wlsunset swaybg|| { echo "Package installation failed."; exit 1; }
 # Services
 sudo xbps-install -Sy dbus avahi cups cronie elogind dunst polkit-gnome|| { echo "Package installation failed."; exit 1; }
 # Terminal
 sudo xbps-install -Sy alacritty foot fastfetch neovim zig go rust fzf zoxide starship btop himalaya zathura || { echo "Package installation failed."; exit 1; }
 # Audio and extras
-sudo xbps-install -Sy pipeWire alsa-pipewire libjack-pipewire wireplumber libpulseaudio || { echo "Package installation failed."; exit 1; }
+sudo xbps-install -Sy pipeWire alsa-pipewire libjack-pipewire wireplumber libpulseaudio qutebrowser || { echo "Package installation failed."; exit 1; }
 
 
 # Install void repo
